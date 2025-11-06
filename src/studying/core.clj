d206f5b93c74079ec135fab35a1dd24d91b23bd5
Igor Martins <igor.martins.silverio@gmail.com>
2025-11-05 23:04:38 -0300

    chore: studying higher order function in clojure

--- src/studying/core.clj
+++ src/studying/core.clj
@@ -10,3 +10,16 @@
       (if (<= valor-pedido 200)
         frete-medio
         frete-gratis))))
+
+
+(defn consulta-taxa-atual
+  []
+  0.2)
+
+(defn imposto-retido
+  [consulta-taxa-padrao salario]
+  (if (< salario 1000)
+    0
+    (* salario (consulta-taxa-padrao))))
+
+(imposto-retido consulta-taxa-atual 1000)
